de693ea028fd1cedb0edf4c2cb66134d7a666f11
Fiama <fiamascollo06@gmail.com>
2023-10-05 00:42:06 -0300

    Agregando modificaciones

--- src/Componentes/Login.jsx
+++ src/Componentes/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
                     Para ingresar a la base de datos de nuestros clientes, debes ingresar el usuario y contraseña.
                 </h2>
                 <h3>
-                    Si desconoces esta información, hacé click en el asistente virtual. Él te indicará dichos datos.
+                    Si desconoces esta información, hacé click en el asistente virtual y deslizate hacia la última pregunta.
                 </h3>
             </div>
             <Form onSubmit={handleSubmit(onSubmit)}>
