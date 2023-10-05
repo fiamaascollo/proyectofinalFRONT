d504de3022a385b324051522a6190461ee2c47c3
Fiama <fiamascollo06@gmail.com>
2023-10-04 23:11:02 -0300

    Arreglando

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css'
 import Navegacion from './Componentes/Navegacion';
 import Home from './Componentes/Home';
 import Vehiculos from './Componentes/Vehiculos';
@@ -9,25 +9,25 @@ import Clientes from './Componentes/Clientes';
 import Footer from './Componentes/Footer';
 import Chatbot from './Componentes/Chatbot';
 import Update from './Componentes/Update';
-import { BrowserRouter as Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
+  return(
     <>
       <Navegacion />
       <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/vehiculos" element={<Vehiculos />} /> 
-        <Route path="/concesionarios" element={<Concesionarios />} />
-        <Route path="/contacto" element={<Contacto />} /> 
-        <Route path="/login" element={<Login />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-        <Route path="/update" element={<Update />} />
+        <Route path="/" element={ <Home />} /> 
+        <Route path="/vehiculos" element={ <Vehiculos />} /> 
+        <Route path="/concesionarios" element={ <Concesionarios />} />
+        <Route path="/contacto" element={ <Contacto />} /> 
+        <Route exact path="/login" element={ <Login />} />
+        <Route path="/clientes" element={ <Clientes />} />
+        <Route path="/chatbot" element={ <Chatbot />} />
+        <Route path="/update" element={ <Update />} />
       </Routes>
       <Footer />
     </>
-  );
+  )
 }
 
 export default App;
